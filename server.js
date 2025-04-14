df64173d4fd226006721be491d2189ea1fcd8a90
khongela <khongela7@gmail.com>
2025-04-14 04:14:37 +0200

    Update including routing /

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const app = express();
 
 //Middleware setup
 app.use(express.static('public'));
-//app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
 app.use(express.json());
 app.use(morgan('combined'));
 app.use(cors());
