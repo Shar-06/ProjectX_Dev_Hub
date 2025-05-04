e1a31f3571188bd019fe9915bf9c8cd41ca38ec9
khongela <khongela7@gmail.com>
2025-05-05 01:57:41 +0200

    please deploy

--- src/api/routes/email.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-
-router.post('/send-welcome-email', emailController.sendWelcomeEmail);
-
-module.exports = router;
