8ee8ac1224da852aeaa8e1b6c48fd237850d179a
khongela <khongela7@gmail.com>
2025-05-05 14:39:13 +0200

    removed db.test

--- src/__tests__/database.test.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const client = require('../config/database');
-
-test('Database connects successfully', async () => {
-  const res = await client.query('SELECT 1');
-  expect(res).toBeDefined();
-  expect(res.rowCount).toBe(1);
-});
